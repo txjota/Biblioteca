490c6f7e9b21f2a93886cb266afd171eafa0a3f8
txjota <otxjota@gmail.com>
2023-12-19 23:55:50 -0400

    Primeira versão

--- obj/Debug/netcoreapp3.1/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0243f83f9697eb2ba9e2583e0730244af0206919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb67edbde5403cb5dcd624289db59057e44a0317")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
